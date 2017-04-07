9311a4789a6d22bcb2c5ce76cc6c3ef9672102b6
Pete Rossman <pete.rossman@gmail.com>
2017-04-07 13:09:15 -0400

    First commit

---
+++ GC_Lab9_CircleCalcs/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lab9_CircleCalcs
+{
+    class Circle
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            this._radius = radius;
+        }
+
+        public double GetCircumference() => 2 * Math.PI * _radius;
+
+        public string GetFormattedCircumference() => String.Format("{0:F2}", GetCircumference());
+
+        public double GetArea() => Math.PI * Math.Pow(_radius, 2);
+
+        public string GetFormattedArea() => String.Format("{0:F2}", GetArea());
+
+
+    }
+}
---
+++ GC_Lab9_CircleCalcs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lab9_CircleCalcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius = 0;
+            Console.WriteLine("Welcome to the Circle Tester!");
+            Console.Write("Enter radius: ");
+            
+            try
+            {
+                radius = int.Parse(Console.ReadLine());
+                if (radius <= 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You did not put in a number! Please try again.");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("You did not put in a number larger than 0! Please try again.");
+            }
+            Circle round = new Circle(radius);
+
+            Console.WriteLine("Circumference: " + round.GetFormattedCircumference());
+            Console.WriteLine("Area: " + round.GetFormattedArea());
+            Console.ReadLine();
+        }
+
+        }
+    }
+
